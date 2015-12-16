32f66179e1b9d1189467321ff06fa5e207ee720d
Say Sotheara <Sotheara@Sothearas-Mac.local>
2015-12-16 14:47:00 +0900

    fixed build setting

--- src/android/libs/before_build.js
+++ src/android/libs/before_build.js
@@ -4,8 +4,8 @@ module.exports = function(context) {
 	var path = require('path');
 
 	var rootDir = process.cwd();
-	var src = path.join(__dirname, 'settings.gradle');
-	var dest = path.join(rootDir, 'platforms/android/settings.gradle');
+	var src = path.join(__dirname, 'build-extras.gradle');
+	var dest = path.join(rootDir, 'platforms/android/build-extras.gradle');
 
 	fs.createReadStream(src).pipe(fs.createWriteStream(dest));
 
