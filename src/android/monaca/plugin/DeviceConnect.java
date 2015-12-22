6136a1c0eb091c40bdcde73b6d5058a7adb220b4
Say Sotheara <Sotheara@Sothearas-Mac.local>
2015-12-22 19:55:58 +0900

    fixed error after updating to the latest core libs

--- src/android/monaca/plugin/DeviceConnect.java
+++ src/android/monaca/plugin/DeviceConnect.java
@@ -5,13 +5,18 @@
 
 package monaca.plugin;
 
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
+import android.os.RemoteException;
+import android.widget.Toast;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.deviceconnect.android.manager.DConnectLaunchActivity;
-import org.deviceconnect.android.manager.DConnectService;
+import org.deviceconnect.android.manager.IDConnectService;
 import org.deviceconnect.android.manager.setting.DevicePluginListActivity;
 import org.deviceconnect.android.manager.setting.SettingActivity;
 import org.json.JSONArray;
@@ -42,11 +47,21 @@ public class DeviceConnect extends CordovaPlugin {
             cordova.getActivity().startActivity(intent);
 
         } else if (action.equals(START)) {
-            cordova.getActivity().startService(new Intent(context, DConnectService.class));
-
+            if (mDConnectService != null) {
+                try {
+                    mDConnectService.start();
+                    Toast.makeText(context, "Server Started...", Toast.LENGTH_SHORT).show();
+                } catch (RemoteException e) {
+                }
+            }
         } else if (action.equals(STOP)) {
-            cordova.getActivity().stopService(new Intent(context, DConnectService.class));
-
+            if (mDConnectService != null) {
+                try {
+                    mDConnectService.stop();
+                    Toast.makeText(context, "Server Stopped...", Toast.LENGTH_SHORT).show();
+                } catch (RemoteException e) {
+                }
+            }
         } else if (action.equals(SETTING)) {
             Intent intent = new Intent(context, SettingActivity.class);
             cordova.getActivity().startActivity(intent);
@@ -60,5 +75,28 @@ public class DeviceConnect extends CordovaPlugin {
         }
         return true;
     }
+
+    @Override
+    public void onResume(boolean multitasking) {
+        super.onResume(multitasking);
+        Intent bindIntent = new Intent(IDConnectService.class.getName());
+        bindIntent.setPackage(cordova.getActivity().getPackageName());
+        cordova.getActivity().bindService(bindIntent, mServiceConnection, Context.BIND_AUTO_CREATE);
+
+    }
+
+    private IDConnectService mDConnectService;
+
+    private final ServiceConnection mServiceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(final ComponentName name, final IBinder service) {
+            mDConnectService = (IDConnectService) service;
+        }
+
+        @Override
+        public void onServiceDisconnected(final ComponentName name) {
+            mDConnectService = null;
+        }
+    };
 
 }
